32b24638cef2c04bbefbfb4331c998ec63575665
Administrator <1657139917@qq.com>
2021-07-16 11:11:15 +0800

    test

--- src/main/java/com/huateng/App.java
+++ src/main/java/com/huateng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "31H111ello World!" );
+        System.out.println( "313H111ello World!" );
     }
 }
